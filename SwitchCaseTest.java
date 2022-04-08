a4c0b2f0427d124754e06bc2ebef94ac3e02536b
ASHUTOSH KUMAR TIWARI <41006524+ashutosh-ceg@users.noreply.github.com>
2022-04-08 18:12:45 +0530

    Add files via upload

---
+++ SwitchCaseTest.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class SwitchCaseTest {
+	public static void main (String[] args) {
+		System.out.println(20);
+		int a=2, b=20;
+		switch(a){
+		    case 1:
+		    System.out.println("Case 1");
+		    break;
+		    case 2:
+		     System.out.println("Case 2");
+		     default:
+		     System.out.println("default");
+		}
+		
+	}
+}
