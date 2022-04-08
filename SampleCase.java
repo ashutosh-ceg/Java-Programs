d3dbf5e8fb946a0244f89419deb97d7299d06e68
ASHUTOSH KUMAR TIWARI <41006524+ashutosh-ceg@users.noreply.github.com>
2022-04-08 18:24:41 +0530

    Add files via upload

---
+++ SampleCase.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+class SampleCase {
+	public static void main (String[] args) {
+		System.out.println(20);
+		int a=2;
+		     c1 :
+		     {
+		         System.out.println("In begining.");
+		         System.out.println("In case c1");
+		         if (a==2)
+		         break c1;
+		         System.out.println("End");
+		     }
+		
+	}
+}
